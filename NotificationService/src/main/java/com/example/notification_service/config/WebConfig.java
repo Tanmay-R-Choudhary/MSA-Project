abaacb8a864de249b14a4e48e4b63d241042cff0
tanmay_choudhary <70155886+Tanmay-R-Choudhary@users.noreply.github.com>
2025-03-03 19:48:58 +0530

    updated NotificationService to support Jwt Token

---
+++ NotificationService/src/main/java/com/example/notification_service/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.notification_service.config;
+
+import com.example.notification_service.security.JwtInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/notifications/**");
+    }
+}
