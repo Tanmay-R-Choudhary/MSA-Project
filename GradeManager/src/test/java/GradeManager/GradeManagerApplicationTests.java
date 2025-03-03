9f99f70d9b318e8aa5621aed27e01a2be836dca9
Vedansh Paliwal <66903973+VedanshPaliwal88@users.noreply.github.com>
2025-03-03 16:52:02 +0530

    changed the user, grade microservice to their project

---
+++ GradeManager/src/test/java/GradeManager/GradeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package GradeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
