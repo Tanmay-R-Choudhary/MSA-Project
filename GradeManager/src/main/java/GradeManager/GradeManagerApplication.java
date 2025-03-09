c5a28c9edb73f269c41c3bab41f87d98cec1408c
Vedansh Paliwal <66903973+VedanshPaliwal88@users.noreply.github.com>
2025-03-09 15:04:11 +0530

    Eureka client for service discovery

--- GradeManager/src/main/java/GradeManager/GradeManagerApplication.java
+++ GradeManager/src/main/java/GradeManager/GradeManagerApplication.java
@@ -2,16 +2,20 @@ package GradeManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GradeManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradeManagerApplication.class, args);
 	}
 
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
